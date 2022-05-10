a1d36edb7988b9d0e6947ade654b8759eb246791
Brian21T <102613002+Brian21T@users.noreply.github.com>
2022-05-10 11:06:19 -0400

    Delete rtc.py

--- rtc.py
+++
@@ -1,9 +0,0 @@
-from machine import RTC
-import time
-
-rtc = RTC()
-rtc.datetime((2017, 8, 23, 2, 12, 48, 0, 0)) # set a specific date and
-                                             # time, eg. 2017/8/23 1:12:48
-while True:
-    print(rtc.datetime()) # get date and time
-    time.sleep(1)    
